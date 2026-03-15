c63d06697af64c27f4be83e789058265e5db67d3
import-hammad <hammadmalik67000@gmail.com>
2026-03-15 15:40:31 +0500

    done

--- script.groovy
+++ script.groovy
@@ -1,17 +1,5 @@
-def buildjar() {
-    echo 'building the application...'
-        sh 'mvn package'
-}
 
-def buildimage() {
-    echo  "building  the docker image"
-                    withCredentials([usernamePassword(credentialsId:'docker-hub-repo',  passwordVariable: 'PASS', usernameVariable: 'USER')]){
-                        sh  'docker build -t piratehammad/demo-app:jma-2.0 .'
-                        sh "echo  $PASS  | docker login -u $USER --password-stdin"
-                        sh 'docker push piratehammad/demo-app:jma-2.0'
-                    }
-}
-def deployapp() {
+def deployApp() {
     echo 'deploying the application...'
 }
 
